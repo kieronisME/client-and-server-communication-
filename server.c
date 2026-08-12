dddc26ff04dc5b4b9550e1a1e81083246e4566da
kieronisME <Kierontekumu2@gmail.com>
2026-08-12 21:56:55 +0100

    tried to add more than one client connection. doesnt work

--- server.c
+++ server.c
@@ -8,28 +8,38 @@
 #define RECV_BUFFER_SIZE 50000
 
 
+
 int main (){
+    int client_count     = 0;
+    int max_client_count = 3;
     int return_value;
     WSADATA data;
     return_value = WSAStartup(MAKEWORD(2,2), &data);
-    if(return_value != 0){
+    if(return_value != 0)
+    {
         printf("[ERROR] WSAstart up failed returning: %d\nWSAGetLastError returned: %d ", return_value, WSAGetLastError());
         return -1;
-    }else{
+    }else
+    {
         printf("[SUCSESS] WSAStartup initiated\n");
     }
     struct sockaddr_in s_server;
     struct sockaddr_in s_client;
 
 
-    //socket creation
+
+
+
+    //Socket creation
     SOCKET server_socket = SOCKET_ERROR;
     SOCKET client_socket = SOCKET_ERROR;
     server_socket = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
-    if(server_socket == INVALID_SOCKET){
+    if(server_socket == INVALID_SOCKET)
+    {
         printf("[ERROR] socket creation failed returning: %d\nWSAGetLastError returned: %d ", INVALID_SOCKET, WSAGetLastError());
         return -1;
-    }else{
+    }else
+    {
         printf("[SUCSESS] socket created\n");
     }
 
@@ -38,68 +48,118 @@ int main (){
     s_server.sin_port        = htons(PORT);
     s_server.sin_addr.s_addr = inet_addr(ADDRESS);
 
+
+
+    //Bind 
     return_value = bind(server_socket, (struct sockaddr*)&s_server, sizeof(s_server));
-    if(return_value == SOCKET_ERROR){
+    if(return_value == SOCKET_ERROR)
+    {
         printf("[ERROR] binding address to socket failed returning: %d\nWSAGetLastError returned: %d ", INVALID_SOCKET, WSAGetLastError());
         return -1;
-    }else{
-        printf("[SUCSESS] sock bound to address.\nListening for client...\n");
+    }else
+    {
+        printf("\ninput how many client this server should accept: ");
+        scanf(" %d", &client_count);
+        if (client_count == max_client_count)
+        {
+            printf("cannot have more than %d clients. Ajusting amount down to 3", max_client_count);
+            client_count = max_client_count;
+        }
+        printf("[SUCSESS] sock bound to address.\nListening for %d clients...\n",client_count);
     }
 
+
+
+
+    int connections[max_client_count];
     int recv_size;
     int client_size;
+    const char* hold    = "\nconnected to server, please wait to send messages...\n";
+    const char* welcome = " welcome to kiki's server";
     const char* message = "server received message\n";
     char recv_data_buffer[RECV_BUFFER_SIZE];
     char client_ip_buffer[20];
 
-    while(1){
+    while(1)
+    {
         // listen for connection
-        return_value = listen(server_socket, 3);
-        if(return_value != 0){
+        return_value = listen(server_socket, max_client_count);
+        if(return_value != 0)
+        {
             printf("[ERROR] listeng failed returning: %d\nWSAGetLastError returned: %d ", return_value, WSAGetLastError());
             return -1;
         }
 
         //accept connetion
-        client_size   = sizeof(struct sockaddr_in);
-        client_socket = accept(server_socket, (struct sockaddr*)&s_client, &client_size);
-        if(client_socket == INVALID_SOCKET){
-            printf("[ERROR] accepting connection on socket _%d_ failed returning: %d\nWSAGetLastError returned: %d ", server_socket, return_value, WSAGetLastError());
-            return -1;
-        }else{
-            printf("[SUCSESS] accepting connection attempts on socket %d\n",server_socket);
-        }
+        for(int x = 0; client_count > x; x++)
+        {
+            client_size   = sizeof(struct sockaddr_in);
+            client_socket = accept(server_socket, (struct sockaddr*)&s_client, &client_size);
+            if(client_socket == INVALID_SOCKET)
+            {
+                printf("[ERROR] accepting connection on socket _%d_ failed returning: %d\nWSAGetLastError returned: %d ", server_socket, return_value, WSAGetLastError());
+                return -1;
+            }else
+            {
+                printf("[SUCSESS] accepting connection attempts on socket %d\n",server_socket);
+                return_value = send(client_socket, hold, strlen(hold), 0);
+                printf("name: %d\n", return_value);
+                if(return_value == SOCKET_ERROR )
+                {
+                    printf("[ERROR] failed to send welcome message to socket _%d_ returning: %d \nWSAGetLastError returned: %d ", client_socket, return_value, WSAGetLastError());
+                    return -1;
+                }
+            }
 
-        //turn client IP to string
-        LPCSTR VALUE = inet_ntop(AF_INET, &(s_client.sin_addr), client_ip_buffer, sizeof(client_ip_buffer));
-        if(VALUE = NULL){
-            printf("[ERROR] failed to turn IPv4 address into string returning: %d\nWSAGetLastError returned: %d ", return_value, WSAGetLastError());
-            return -1;
-        }else{
-            printf("[SUCSESS] IPv4 address converted into a string\n");
+            //turn client IP to string
+            LPCSTR VALUE = inet_ntop(AF_INET, &(s_client.sin_addr), client_ip_buffer, sizeof(client_ip_buffer));
+            if(VALUE = NULL)
+            {
+                printf("[ERROR] failed to turn IPv4 address into string returning: %d\nWSAGetLastError returned: %d ", return_value, WSAGetLastError());
+                return -1;
+            }else
+            {
+                printf("[SUCSESS] IPv4 address converted into a string\n");
+            }
+        } 
+        
+
+        // print welcome messages to clients
+        for(int x = 0; client_count > x; x++ ){
+            return_value = send(client_socket, welcome, strlen(welcome), 0);
+            if(return_value == SOCKET_ERROR )
+            {
+                printf("[ERROR] failed to send welcome message to socket _%d_ returning: %d \nWSAGetLastError returned: %d ", client_socket, return_value, WSAGetLastError());
+                return -1;
+            }
         }
 
 
         //print to client hopfully 
-
-        while (1){
+        while(1)
+        {
             //recviec
             memset(recv_data_buffer, 0, sizeof(recv_data_buffer));
             recv_size  = recv(client_socket, recv_data_buffer, RECV_BUFFER_SIZE, 0);
+
+
             if(recv_size == SOCKET_ERROR){
                 printf("[ERROR] receiving connection from client socket failed returning: %d\nWSAGetLastError returned: %d ", recv_size, WSAGetLastError());
                 break;
 
-            }else if(recv_size > 0){
+            }else if(recv_size > 0)
+            {
                 printf("Client %s: %s\n", client_ip_buffer, recv_data_buffer);
                 recv_data_buffer[recv_size] = '\0';
                 return_value = send(client_socket, message, strlen(message), 0);
-                if(return_value == SOCKET_ERROR ){
+                if(return_value == SOCKET_ERROR )
+                {
                     printf("[ERROR] failed to send data to socket _%d_ returning: %d \nWSAGetLastError returned: %d ", client_socket, return_value, WSAGetLastError());
                     return -1;
                 }
                 
-            }else if(recv_size == 0){
+            }else if(recv_size == 0)
+            {
                 printf("Client %s disconnected\n", client_ip_buffer);
                 break;
 
@@ -112,26 +172,32 @@ int main (){
 
     //clean up
     return_value = closesocket(server_socket);
-    if(return_value == SOCKET_ERROR){
+    if(return_value == SOCKET_ERROR)
+    {
         printf("[ERROR] failed to close server socket returning: %d\nWSAGetLastError returned: %d ", return_value, WSAGetLastError());
         return -1;
-    }else{
+    }else
+    {
         printf("[SUCSESS] server socket is all cleaned up\n");
     }
 
     return_value = closesocket(client_socket);
-    if(return_value == SOCKET_ERROR){
+    if(return_value == SOCKET_ERROR)
+    {
         printf("[ERROR] failed to close client socket returning: %d\nWSAGetLastError returned: %d ", return_value, WSAGetLastError());
         return -1;
-    }else{
+    }else
+    {
         printf("[SUCSESS] client socket is all cleaned up\n");
     }
 
     return_value = WSACleanup();
-    if(return_value == SOCKET_ERROR){
+    if(return_value == SOCKET_ERROR)
+    {
         printf("[ERROR] WSACleanup failed returning: %d\nWSAGetLastError returned: %d ", return_value, WSAGetLastError());
         return -1;
-    }else{
+    }else
+    {
         printf("[SUCSESS] WSACleanup has finnished cleaning\n");
     }
 
